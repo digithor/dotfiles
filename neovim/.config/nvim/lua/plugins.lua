9b676a39cdad2c658f1730847e2f855b0a93458f
Thor Kritsada <thor.kritsada@gmail.com>
2024-08-18 22:28:58 +0700

    Update neovim config

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -9,10 +9,6 @@ return {
 		name = "catppuccin",
 		opts = {
 			flavour = "frappe",
-			inddent_blankline = {
-				enable = true,
-				colored_indent_levels = false,
-			},
 			default_integrations = true,
 			integrations = {
 				cmp = true,
@@ -20,8 +16,37 @@ return {
 				nvimtree = true,
 				telescope = true,
 				treesitter = true,
+				treesitter_context = true,
+				indent_blankline = {
+					enabled = true,
+				},
 				mini = {
 					enabled = true,
+				},
+				lsp_trouble = true,
+				native_lsp = {
+					enabled = true,
+					virtual_text = {
+						errors = { "italic" },
+						hints = { "italic" },
+						warnings = { "italic" },
+						information = { "italic" },
+						ok = { "italic" },
+					},
+					underline = {
+						errors = { "underline" },
+						hints = { "underline" },
+						warnings = { "underline" },
+						information = { "underline" },
+						ok = { "underline" },
+					},
+					inlay_hints = {
+						background = true,
+					},
+				},
+				illuminate = {
+					enabled = true,
+					lsp = true,
 				},
 			},
 		},
