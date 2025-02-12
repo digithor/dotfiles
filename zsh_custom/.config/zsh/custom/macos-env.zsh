62ef252d3441921fb2d2a4deafd522821a80eeaf
Thor Kritsada <thor.kritsada@gmail.com>
2025-02-12 22:44:09 +0700

    Update macos-env.zsh file

--- zsh_custom/.config/zsh/custom/macos-env.zsh
+++ zsh_custom/.config/zsh/custom/macos-env.zsh
@@ -4,9 +4,11 @@ export PATH="$HOMEBREW_PREFIX_CURL/bin:$PATH"
 export PATH="$(brew --prefix ruby)/bin:$PATH"
 export PATH="$(brew --prefix python)/libexec/bin:$PATH"
 export PATH="$PATH:$HOME/Library/Application Support/Coursier/bin"
+export PATH="$(brew --prefix gawk)/libexec/gnubin:$PATH"
 
 export LDFLAGS="-L$HOMEBREW_PREFIX_CURL/lib"
 export CPPFLAGS="-I$HOMEBREW_PREFIX_CURL/include"
 export PKG_CONFIG_PATH="$HOMEBREW_PREFIX_CURL/lib/pkgconfig"
 
 export GROOVY_HOME="$(brew --prefix groovy)/libexec"
+
