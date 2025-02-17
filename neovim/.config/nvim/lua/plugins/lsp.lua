baa4adca6adb289ff80ef7694ade33419164b3be
Thor Kritsada <thor.kritsada@gmail.com>
2025-02-17 15:04:11 +0700

    Update nvim config

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -1,32 +1,47 @@
 return {
-	"neovim/nvim-lspconfig",
-	event = { "BufReadPre", "BufNewFile" },
-	dependencies = {
-		{
-			"folke/neoconf.nvim",
-			cmd = "Neoconf",
-			config = true,
-		},
-		{ "folke/neodev.nvim", opts = {} },
+	{
+		"b0o/schemastore.nvim",
 	},
-	config = function()
-		local lspconfig = require("lspconfig")
-		lspconfig.lua_ls.setup({})
-		lspconfig.terraformls.setup({})
-		lspconfig.rust_analyzer.setup({})
-		lspconfig.gopls.setup({})
-		lspconfig.pyright.setup({})
-		lspconfig.ruff.setup({})
-		lspconfig.yamlls.setup({})
-		lspconfig.ansiblels.setup({})
-		lspconfig.denols.setup({})
-		lspconfig.kotlin_language_server.setup({})
-		lspconfig.jdtls.setup({})
-		lspconfig.solargraph.setup({})
-		lspconfig.ts_ls.setup({})
-		lspconfig.tflint.setup({})
-		lspconfig.tailwindcss.setup({})
-		lspconfig.bashls.setup({})
-		lspconfig.eslint.setup({})
-	end,
+	{
+		"neovim/nvim-lspconfig",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			{
+				"folke/neoconf.nvim",
+				cmd = "Neoconf",
+				config = true,
+			},
+			{ "folke/neodev.nvim", opts = {} },
+		},
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+			lspconfig.terraformls.setup({})
+			lspconfig.rust_analyzer.setup({})
+			lspconfig.gopls.setup({})
+			lspconfig.pyright.setup({})
+			lspconfig.ruff.setup({})
+			lspconfig.yamlls.setup({
+				settings = {
+					yaml = {
+						schemaStore = {
+							enable = false,
+							url = "",
+						},
+						schemas = require("schemastore").json.schemas(),
+					},
+				},
+			})
+			lspconfig.ansiblels.setup({})
+			lspconfig.denols.setup({})
+			lspconfig.kotlin_language_server.setup({})
+			lspconfig.jdtls.setup({})
+			lspconfig.solargraph.setup({})
+			lspconfig.ts_ls.setup({})
+			lspconfig.tflint.setup({})
+			lspconfig.tailwindcss.setup({})
+			lspconfig.bashls.setup({})
+			lspconfig.eslint.setup({})
+		end,
+	},
 }
