4a33a7335e0a74bb61ec3487b59c1ee8cc7bf336
Kritsada Chinpala <thor.kritsada@gmail.com>
2026-01-09 22:22:09 +0700

    [zsh] Remove Homebrew command-not-found handler

--- zsh_custom/.config/zsh/custom/env.zsh
+++ zsh_custom/.config/zsh/custom/env.zsh
@@ -44,10 +44,5 @@ Darwin)
     export GROOVY_HOME="$HOMEBREW_PREFIX/opt/groovy/libexec"
 
     export PATH="$HOME/.cargo/bin:$HOMEBREW_PREFIX/opt/rustup/bin:$PATH"
-
-    HOMEBREW_COMMAND_NOT_FOUND_HANDLER="$(brew --repository)/Library/Homebrew/command-not-found/handler.sh"
-    if [ -f "$HOMEBREW_COMMAND_NOT_FOUND_HANDLER" ]; then
-        source "$HOMEBREW_COMMAND_NOT_FOUND_HANDLER"
-    fi
     ;;
 esac
