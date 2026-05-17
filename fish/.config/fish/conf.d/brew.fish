1ebbe00009793c8e9189020638d6a4494dd29d3f
Kritsada Chinpala <thor.kritsada@gmail.com>
2026-05-17 21:47:05 +0700

    [fish] Configure Fish shell environment and tools

---
+++ fish/.config/fish/conf.d/brew.fish
@@ -0,0 +1,4 @@
+switch (uname -s)
+    case 'Darwin'
+        eval (/opt/homebrew/bin/brew shellenv)
+end
---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+# Commands to run in interactive sessions can go here
+end
+
+starship init fish | source
+
+# Set up fzf key bindings
+fzf --fish | source
